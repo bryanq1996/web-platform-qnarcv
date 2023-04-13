106fdf49f4b6bc06c61ed07bc8c01e86fc8baccf
bryanq1996 <117610906+bryanq1996@users.noreply.github.com>
2023-04-12 23:24:15 -0500

    1 file modified

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
