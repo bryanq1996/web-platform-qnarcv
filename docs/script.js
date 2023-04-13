2c5e5210de4e5c91ea31b9c155a3ddde14533e87
bryanq1996 <117610906+bryanq1996@users.noreply.github.com>
2023-04-13 07:25:00 -0500

    2 files modified

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,15 @@
 consola.log('UPS ECUADOR');
 
-
+var i=0;
 
 var elemento = document.getElementById("titulo1")
 elemento,addEventListener("click", function(){
     alert("click en titulo");
+})
+
+
+ elemento,addEventListener("mouseover", function(){
+     i = i +1
+     console.log(i)
+     elemento.innerHTML = i
 })
